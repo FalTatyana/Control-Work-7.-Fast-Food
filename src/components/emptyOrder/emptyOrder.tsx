340059d07381be3d7e08cb598439257c9fd18c08
Фаль Татьяна <tatty.fal@gmail.com>
2026-05-09 17:26:32 +0600

    Created a component for empty order. Code refactoring was performed

---
+++ src/components/emptyOrder/emptyOrder.tsx
@@ -0,0 +1,10 @@
+const EmptyOrder = () => {
+    return (
+        <>
+            <p className="span"><b>Order is empty!</b></p>
+            <span className="span">Please add some items.</span>
+        </>
+    );
+};
+
+export default EmptyOrder
